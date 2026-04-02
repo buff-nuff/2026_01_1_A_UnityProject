4f900180148079152852b7fa4535dae28fdb1fb7
buff-nuff <xogkswjd@g.shingu.ac.kr>
2026-04-02 12:25:04 +0900

    [Feat] void game

---
+++ 2026_01_1_A_UnityProject/Assets/Prefabs/PlayerHealth.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxLives = 3;
+    public int currentLives;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentLives = maxLives;
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+       if (other.CompareTag("Missile"))
+        {
+            currentLives--;
+            Destroy(other.gameObject);
+
+            if (currentLives <= 0)
+            {
+                GameOver();
+            }
+        } 
+    }
+
+
+    void GameOver()
+    {
+        gameObject.SetActive(false);
+        Invoke("RestarGame", 3.0f);
+    }
+
+    void RestarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
